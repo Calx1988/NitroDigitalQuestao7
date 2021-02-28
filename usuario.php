2990160a4a1310c5d7db4f90bb439271141cfef5
Cássio Alexsander <63707638+Calx1988@users.noreply.github.com>
2021-02-28 11:36:25 -0300

    Throw exception implemented with flaws.
    Erro para validar o $nível. Não conseguindo referenciar a função getNivel().
    Try and Catch não funcionam. Necessário melhorar o tratamento de erros. Mensagem não amigável para o usuário.

--- usuario.php
+++ usuario.php
@@ -22,18 +22,57 @@ class Usuario{
     }
 
 
-    function preencherDados($nome, $telefone, $nivel){
+    public function preencherDados($nome, $telefone, $nivel){
         $this->nome=$nome;
         $this->telefone=$telefone;
         $novaPermissao=new Permissao();
         $novaPermissao->setNivel($nivel);
     }
     function cadastrar(){
-        if(is_empty($nome) || p)
-    }
-   
+        if(empty($this->nome)){
+            throw new Exception("Nome deve ser preenchido!");
+        }else{
+            echo $this->getNome();
+        }
+/*
 
-    
+        // ESSES TRY E CATCH DEVEM ESTAR NO LOCAL ERRADO. PRECISO ESTUDAR ISSO!
+
+        try{
+            $this->cadastrar();
+            echo $this->getNome;
+        }
+        catch(Exception $e){
+            echo 'Message: '.$e->getMessage();
+        }
+*/
+        if(empty($this->telefone)){
+            throw new Exception("Número de telefone deve ser preenchido!");
+        }else{
+            echo $this->getTelefone();
+        }
+/*        
+        try{
+            $this->cadastrar();
+            echo $this->getTelefone;
+        }
+        catch(Exception $e){
+            echo 'Message: '.$e->getMessage();
+        }
+*/        
+                   
+/*
+        // COMO VALIDAR O NIVEL? PRECISO REFERENCIAR O GETNIVEL() DO OBJ $NOVAPERMISSAO. PENSANDO...
+
+        if(empty($novaPermissao->getNivel())){
+            throw new Exception("Nível deve ser preenchido!");
+        }else{
+            echo $this->getTelefone();
+        }
+*/
+        
+    }
+        
 
 }
 
