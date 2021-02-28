80aa593ebe3cb5af5311f7c1ffefca91b6196f3a
Cássio Alexsander <63707638+Calx1988@users.noreply.github.com>
2021-02-28 11:32:35 -0300

    Updated index.php. Function cadastrar() used.

--- index.php
+++ index.php
@@ -3,15 +3,5 @@
     
     $usuario1= new Usuario();
     $usuario1->preencherDados('Fulano de Tal', '51963852741', 'Caixa');
-    echo $usuario1->getNome();
-    echo $usuario1->getTelefone();
-    
+    echo $usuario1->cadastrar();
 ?>
-
-<html lang="pt-br">
-    <meta charset="udf-8">
-    <head>Cadastro de Usuário</head>
-
-
-
-</html>
